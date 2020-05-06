24e25803f614ce8934a610adf6610d127eb48ffc
Chen Shou <cshou@google.com>
2020-05-06 21:06:46 +0000

    default json encoding

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/cloudevents/sdk-go/v2 v2.0.0-preview8
 	github.com/golang/protobuf v1.4.0
-	github.com/google/go-cmp v0.4.0
+	github.com/google/go-cmp v0.4.0 // indirect
 	github.com/google/uuid v1.1.1
-	google.golang.org/protobuf v1.21.0
+	google.golang.org/protobuf v1.22.0
 )
--- pkg/ceproto/codec.go
+++ pkg/ceproto/codec.go
@@ -1,44 +1,85 @@
 package ceproto
 
 import (
-	"log"
+	"fmt"
 	"strings"
 
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/google/uuid"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 func Marshal(msg proto.Message, event *event.Event) error {
-	b, err := proto.Marshal(msg)
-	if err != nil {
+	if err := setData(msg, event); err != nil {
 		return err
 	}
-	event.SetData("application/octet-stream", b)
 	return setEventAttributes(msg, event)
 }
 
 func Unmarshal(event *event.Event, msg proto.Message) error {
-	if err := proto.Unmarshal(event.Data(), msg); err != nil {
-		return err
+	switch event.DataContentType() {
+	case "application/json":
+		return protojson.Unmarshal(event.Data(), msg)
+	case "application/octet-stream":
+		return proto.Unmarshal(event.Data(), msg)
 	}
+	return fmt.Errorf("content type %s not supported", event.DataContentType())
+}
+
+// func Play(msg proto.Message) {
+// 	mref := msg.ProtoReflect()
+// 	mref.Descriptor().Options().ProtoReflect().Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+// 		log.Printf("Message option name: %q\n", fd.Name())
+// 		log.Printf("Message option value: %q\n", v.String())
+// 		return true
+// 	})
+// 	mref.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+// 		log.Printf("Field json name: %q\n", fd.JSONName())
+// 		log.Printf("Field type: %v\n", fd.Kind())
+// 		log.Printf("Field string value: %v\n", v.String())
+// 		return true
+// 	})
+// }
+
+func setData(msg proto.Message, event *event.Event) error {
+	encoding := getEncoding(msg)
+	switch encoding {
+	case "JSON":
+		b, err := protojson.Marshal(msg)
+		if err != nil {
+			return err
+		}
+		event.SetData("application/json", b)
+	case "PROTO":
+		b, err := proto.Marshal(msg)
+		if err != nil {
+			return err
+		}
+		event.SetData("application/octet-stream", b)
+	default:
+		return fmt.Errorf("encoding %s not supported", encoding)
+	}
+
 	return nil
 }
 
-func Play(msg proto.Message) {
+func getEncoding(msg proto.Message) string {
 	mref := msg.ProtoReflect()
-	mref.Descriptor().Options().ProtoReflect().Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
-		log.Printf("Message option name: %q\n", fd.Name())
-		log.Printf("Message option value: %q\n", v.String())
+	mopts := mref.Descriptor().Options().ProtoReflect()
+	var encoding string
+	mopts.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		if fd.Name() == "transport_encoding" {
+			encoding = v.String()
+		}
 		return true
 	})
-	mref.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
-		log.Printf("Field json name: %q\n", fd.JSONName())
-		log.Printf("Field type: %v\n", fd.Kind())
-		log.Printf("Field string value: %v\n", v.String())
-		return true
-	})
+
+	if encoding == "" {
+		encoding = "JSON"
+	}
+	return encoding
 }
 
 func setEventAttributes(msg proto.Message, event *event.Event) error {
